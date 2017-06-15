9d97b68975fd1ab8e2d87c4f5d5edfc7ce4e586b
Mamadou Diop <madouma@gmail.com>
2017-06-15 02:04:26 -0500

    - Added apollo-client, apollo-angular and graphql dependencies to our app - Create an ApolloClient instance and point it at our GraphQL server - Connect our client instance to the app,

--- AffordableTours/src/app/app.module.ts
+++ AffordableTours/src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { ApolloClient, createNetworkInterface } from "apollo-client";
+import { ApolloModule } from "apollo-angular";
+
 import { AppComponent } from './app.component';
 
+const client = new ApolloClient({
+    networkInterface: createNetworkInterface({
+        uri: 'https://43skynpam2.execute-api.us-east-1.amazonaws.com/devmarc'
+    })
+})
+
+export function provideClient(): ApolloClient {
+    return client;
+}
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        ApolloModule.forRoot(provideClient)
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
